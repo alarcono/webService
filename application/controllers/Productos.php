fe2ee2193b5a9be84ed2fc9fc0d110aba75b8139
Tamara Alarcón <talarcon@dreamit.cl>
2017-11-02 15:49:26 -0300

    Primer commit

---
+++ application/controllers/Productos.php
@@ -0,0 +1,23 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
+require APPPATH . 'libraries/REST_Controller.php';
+
+class Productos extends REST_Controller
+{
+  public function index_get()
+  {
+
+  }
+
+  public function search_get(){
+
+  		
+  }
+
+
+}
+
